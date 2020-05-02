7e77d2ac342d0e3134af08a00148d77838058592
arfa <arfrix.dev@gmail.com>
2020-05-02 13:37:12 +0430

    add images and conditions to chair

--- src/table/chair/chair.js
+++ src/table/chair/chair.js
@@ -1,12 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './chair.css'
 
 
-function chair (){
-    return(
 
-    )
+function Chair(){
+    const [isFree , setIsFree] = useState(true)
+    const [ifReserved , setIfReserved] = useState(false)
+    return(
+        <div className >
+            {isFree ?
+            <img src="assets/img/free_chair.png" className=""></img> :
+            ifReserved ? <img src="assets/img/used_chair.png" className=""></img> :
+            <img src="assets/img/reserved_chair.png" className=""></img> }
+        </div>
+    );
 }
 
 
-export default chair;
+export default Chair;
